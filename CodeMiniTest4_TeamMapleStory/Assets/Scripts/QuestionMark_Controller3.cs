8106aab3f224c387c94f1327d0b2f4096788356b
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2020-12-13 22:50:04 +0800

    Complete level 2

---
+++ CodeMiniTest4_TeamMapleStory/Assets/Scripts/QuestionMark_Controller3.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuestionMark_Controller3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Level 3");
+            SceneManager.LoadScene("Level 3");
+        }
+    }
+}
